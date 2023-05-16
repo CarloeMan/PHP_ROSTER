04143f1def2f49bf0e35fe3fb5d58fc682c5cacd
orsuman <den8weekgold@gmail.com>
2023-05-16 22:18:20 +0900

    前月・翌月表示切替対応

--- kinmu.js
+++ kinmu.js
@@ -1,16 +1,44 @@
-function workStart() {
-    document.getElementsByName("startFlg")[0].value = "1";
-    confirmDialog();
-    frmSubmit();
-}
+const ON_FLG = "1";
+// 前月／翌月ボタン押下時
+function monthChange(agoflg) {
+    let dispYM = document.getElementsByName("dispYM")[0].value;
+    let dispYear  = dispYM.substr(0, 4);  // 表示年
+    let dispMonth = dispYM.substr(4);     // 表示月
+    let intMonth = Number.parseInt(dispMonth);
+    if (agoflg) {
+        intMonth = intMonth - 1;
+    } else {
+        intMonth = intMonth + 1;
+    }
+    if (intMonth < 1 || intMonth > 12 ) {
+        let intYear = Number.parseInt(dispYear);
+        if (intMonth < 1) {
+            intYear = intYear - 1;
+            intMonth = 12;
+        } else {
+            intYear = intYear + 1;
+            intMonth = 1;
+        }
+        dispYear = intYear.toString();
+    } 
+    if (intMonth < 10) {
+        dispMonth = "0" + intMonth.toString();
 
-function workEnd() {
-    document.getElementsByName("endFlg")[0].value = "1";    
-    confirmDialog();
-    frmSubmit();
+    } else {
+        dispMonth = intMonth.toString();
+    }
+    dispYM = dispYear + dispMonth;
+    document.getElementsByName("dispYM")[0].value = dispYM;
+    document.kinmu.submit();
 }
-
+// 画面表示時
 function onLoad() {
+    let dispYM = document.getElementsByName("dispYM")[0].value;
+    let nowYM = document.getElementsByName("nowYM")[0].value;
+    if (dispYM != nowYM) {
+        document.getElementsByName("btnStart")[0].style.display ="none"; // 業務開始ボタン
+        document.getElementsByName("btnEnd")[0].style.display ="none";   // 業務終了ボタン
+    }
     if (document.getElementsByName("startFlg")[0].value == "1") {
         document.getElementsByName("startSumiFlg")[0].value = "1";
         document.getElementsByName("startFlg")[0].value = "";
@@ -20,16 +48,22 @@ function onLoad() {
         document.getElementsByName("endFlg")[0].value = "";    
     }
 }
-
-// submit
-function frmSubmit() {
-    document.kinmu.submit();
-}
-
-// 
-function confirmDialog() {
-    let flg = window.confirm("勤務時間を確定します。\n確定後は変更できません。よろしいですか？");
-    if(!flg) {
+// formタグ内のパラメータ値submit
+function frmSubmit(startflg) {
+    let msg = "勤務";
+    if (startflg) {
+        msg += "開始"; 
+    } else {
+        msg += "終了"; 
+    }
+    msg += "時間を確定します。\n確定後は変更できません。よろしいですか？";
+    if (!window.confirm(msg)) {
         return;
     }
+    if (startflg) {
+        document.getElementsByName("startFlg")[0].value = ON_FLG;
+    } else {
+        document.getElementsByName("endFlg")[0].value = ON_FLG;
+    }
+    document.kinmu.submit();
 }
--- kinmu.php
+++ kinmu.php
@@ -10,9 +10,34 @@
 <body onload="onLoad();">
 <form name="kinmu" action="kinmu.php" method="POST">
 <?php
-// 定数
-const MAX_DAYS = 31; // １か月に存在する最大の日数
-  
+// 表示する曜日の作成
+function dispYoubi(String $strNum) : string  {
+    $intNum = (int)$strNum;
+    switch ($intNum) {
+      case 0 : 
+        return '日';
+        break;
+      case 1 : 
+        return '月';
+        break;
+      case 2 : 
+        return '火';
+        break;  
+      case 3 : 
+        return '水';
+        break;  
+      case 4 : 
+        return '木';
+        break;  
+      case 5 : 
+        return '金';
+         break;  
+      case 6 : 
+        return '土';
+        break;  
+    }
+}
+
 // 表示する勤務表の作成
 function makeKinmuhyoHtml(array $list) : void  {
     $startFlg = $list["startFlg"];
@@ -22,21 +47,26 @@ function makeKinmuhyoHtml(array $list) : void  {
     $strYear = $list["strYear"];
     $strMonth = $list["strMonth"];
     $strYM = $strYear.$strMonth;
+    $maxDay = date('t',strtotime($strYM));  // 1か月の日数
     $intYear  = (int)$strYear;
     $intMonth = (int)$strMonth;
     // カレンダー日付の作成
-    for ($intDay=1; $intDay<=MAX_DAYS; $intDay++) {
+    for ($intDay=1; $intDay<=$maxDay; $intDay++) {
         if (checkDate($intMonth, $intDay, $intYear)) {
             $strDay = (String)$intDay;  // int型からstring型に変換
+            $strYMD = $strYM.$strDay;
+            $numYoubi = date('w',strtotime($strYMD));
             $strDay = str_pad($strDay, 2, "0", STR_PAD_LEFT);  // 1桁の場合、前0詰めの2桁表示
             echo '<tr>';
             echo '<td class="dispDay">';
-            echo $strMonth.'月'.$strDay.'日';
+            echo $strMonth.'月'.$strDay.'日 ('.dispYoubi($numYoubi).')';
             echo '</td>';
             echo '<td>';
             echo '<span id="'.$strMonth.$strDay.'start">';
             // 業務開始（本日付）
-            if ($strNowYmd == $strYM.(String)$intDay && $startFlg == '1') {
+            if ($numYoubi == 0 || $numYoubi == 6) {
+              echo $now->format('―');
+            } else if ($strNowYmd == $strYM.(String)$intDay && $startFlg == '1') {
                 echo $now->format('H時i分s秒');
             }
             echo '</span>';
@@ -44,7 +74,9 @@ function makeKinmuhyoHtml(array $list) : void  {
             echo '<td>';
             echo '<span id="'.$strMonth.$strDay.'end">';
             // 業務終了（本日付）
-            if ($strNowYmd == $strYM.(String)$intDay && $endFlg == '1') {
+            if ($numYoubi == 0 || $numYoubi == 6) {
+              echo $now->format('―');
+            } else if ($strNowYmd == $strYM.(String)$intDay && $endFlg == '1') {
                 echo $now->format('H時i分s秒');
             }
             echo '</td>';
@@ -59,52 +91,64 @@ $list;
 // POSTパラメータ取得
 $startFlg = filter_input(INPUT_POST, 'startFlg');
 $endFlg = filter_input(INPUT_POST, 'endFlg');
+$dispYM = filter_input(INPUT_POST, 'dispYM');
+$nowYM = filter_input(INPUT_POST, 'nowYM');
 $list["startFlg"] = $startFlg;
 $list["endFlg"] = $endFlg;
 
 // 日付の取得
-$now = new DateTime();
-$strNowYear  = $now->format('Y');
-$strNowMonth = $now->format('m');
+if ($dispYM == null || $dispYM == "") {
+  $now = new DateTime();
+  $strNowYear  = $now->format('Y');
+  $strNowMonth = $now->format('m');
+  $dispYM = $strNowYear.$strNowMonth;
+  $nowYM = $dispYM;
+} else {
+  $strNowYear  = mb_substr($dispYM, 0, 4);
+  $strNowMonth = mb_substr($dispYM, 4);
+}
 $list["strYear"] = $strNowYear;
 $list["strMonth"] = $strNowMonth;
 ?>
 
 <div style="text-align:center;">
-  <h2>勤怠管理表</h2>
-  <div style="text-align:center;">
-    <button type="button" style="margin-right:20px;">＜前月</button>
-      <span>
+    <h2>勤怠管理表</h2>
+    <div style="text-align:center;">
+      <button type="button" style="margin-right:20px;" onClick="monthChange(true);">＜前月</button>
+        <span>
+  <?php
+          echo $strNowYear.'年'.$strNowMonth.'月';
+  ?>
+        </span>
+      <button type="button" style="margin-left:20px;" onClick="monthChange(false);">翌月＞</button>
+    </div>
+    <div style="margin-top:20px;">
+      <button type="button" name="btnStart" style="margin-right:20px;" onClick="frmSubmit(true);">業務開始</button>
+      <button type="button" name="btnEnd" onClick="frmSubmit(false);">業務終了</button>
+    </div>
+    <!-- 勤務表作成開始 -->
+    <table border="1" class="border" style="margin:auto; margin-top:20px;">
+      <tr>
+        <td width="200px" class="dispDay">日付</td>
+        <td width="200px" class="dispDay">始業時間</td>
+        <td width="200px" class="dispDay">終業時間</td>
+      </tr>
 <?php
-        echo $strNowYear.'年'.$strNowMonth.'月';
+      // 勤務表を表示
+      makeKinmuhyoHtml($list);
 ?>
-      </span>
-    <button type="button" style="margin-left:20px;">翌月＞</button>
+    </table>
+    <!-- 勤務表作成終了 -->
   </div>
-  <div style="margin-top:20px;">
-    <button type="button" style="margin-right:20px;" onClick="workStart();">業務開始</button>
-    <button type="button" onClick="workEnd();">業務終了</button>
-  </div>
-  <!-- 勤務表作成開始 -->
-  <table border="1" class="border" style="margin:auto; margin-top:20px;">
-    <tr>
-      <td width="150px" class="dispDay">日付</td>
-      <td width="200px" class="dispDay">始業時間</td>
-      <td width="200px" class="dispDay">終業時間</td>
-<?php
-    makeKinmuhyoHtml($list); // 勤務表を表示
-?>
-    </tr>
-  </table>
-  <!-- 勤務表作成終了 -->
-</div>
-<!-- 隠し項目（パラメータ）-->
-<input type="hidden" name="startFlg" value="">
-<input type="hidden" name="startSumiFlg" value="">
-<input type="hidden" name="endFlg" value="">
-<input type="hidden" name="endSumiFlg" value="">
-<input type="hidden" name="strYear" value="">
-<input type="hidden" name="strMonth" value="">
+  <!-- 隠し項目（パラメータ）-->
+  <input type="hidden" name="startFlg" value="">
+  <input type="hidden" name="startSumiFlg" value="">
+  <input type="hidden" name="endFlg" value="">
+  <input type="hidden" name="endSumiFlg" value="">
+  <input type="hidden" name="strYear" value="">
+  <input type="hidden" name="strMonth" value="">
+  <input type="hidden" name="dispYM" value="<?php echo $dispYM;?>">
+  <input type="hidden" name="nowYM" value="<?php echo $nowYM;?>">
 </form>
 </body>
 </html>
